ceb4b3f7a8d5a7a1034b6142529b262142aae289
Vladimir Angelov <vladimirsfile@gmail.com>
2026-01-21 23:38:57 +0200

    Add comments on longSession().

--- hobbyTracker.js
+++ hobbyTracker.js
@@ -5,17 +5,17 @@ const hobbyLog = [
   { day: "Thursday", hobby: "drawing", minutes: 25, mood: "creative" },
   { day: "Friday", hobby: "reading", minutes: 35, mood: "calm" }
 ];
-// This function calculates the total time spent on all hobies. 
+// This function calculates the total time spent on all hobbies. 
 // The function accepts "log" as a parameter, which in an array of objects.
 function totalTime(log) {
 // This line has a reduce() to combine every item in the array into a 
 // single value. "sum" is the accumulator, which is set to start at "0" by the 
 // "0" at the end of the line. "session" is the current object, for each loop
-// it adds the minutes of the current objet to the accumolator.
+// it adds the minutes of the current object to the accumulator.
   return log.reduce((sum, session) => sum + session.minutes, 0);
 }
 
-// The function returs all the hobies without duplicates.  
+// The function returns all the hobbies without duplicates.  
 function uniqueHobbies(log) {
 // Here "map" is used to create a new array that contains only the "hobby" names.
   const names = log.map(entry => entry.hobby);
@@ -23,7 +23,11 @@ function uniqueHobbies(log) {
   return [...new Set(names)];
 }
 
+// The function searches array of objects for activities that last longer
+// than what is set by the second parameter "minMinutes". 
 function longSessions(log, minMinutes) {
+// "filter()" filters and keeps only the objects which lasted longer than the
+// value provided by "minMinutes". 
   return log.filter(entry => entry.minutes > minMinutes);
 }
 
